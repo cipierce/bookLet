1bdd8d505ad306800ae314a84eccb2985fc706ef
Gina Stalica <gstalica@Searles128-17.campus.bowdoin.edu>
2016-04-28 15:58:43 -0400

    table views with collection views - MAYBE working

--- BookLet/PersonalUserTableViewCell.swift
+++ BookLet/PersonalUserTableViewCell.swift
@@ -12,20 +12,6 @@ class PersonalUserTableViewCell: UITableViewCell {
     
     @IBOutlet weak var collectionView: UICollectionView!
 
-/*
-    override func awakeFromNib() {
-        super.awakeFromNib()
-        // Initialization code
-    }
-
-    override func setSelected(selected: Bool, animated: Bool) {
-        super.setSelected(selected, animated: animated)
-
-        // Configure the view for the selected state
-    }
-
-*/
-
 }
 
 extension PersonalUserTableViewCell {
--- BookLet/PersonalUserViewController.swift
+++ BookLet/PersonalUserViewController.swift
@@ -17,117 +17,63 @@ class PersonalUserViewController: UITableViewController {
     }
     
     override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCellWithIdentifier("Cell", forIndexPath: indexPath)
+        
+        let cell = tableView.dequeueReusableCellWithIdentifier("Personal User Table View Cell", forIndexPath: indexPath)
         
         return cell
     }
-    /*
+    
     override func tableView(tableView: UITableView, willDisplayCell cell: UITableViewCell, forRowAtIndexPath indexPath: NSIndexPath) {
         guard let tableViewCell = cell as? PersonalUserTableViewCell else { return }
+        
         tableViewCell.setCollectionViewDataSourceDelegate(self, forRow: indexPath.row)
     }
-    */
-
-    /*
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem()
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-    // MARK: - Table view data source
-
-    override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
-        return 0
-    }
-
-    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of rows
-        return 0
-    }
-
-    /*
-    override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCellWithIdentifier("reuseIdentifier", forIndexPath: indexPath)
-
-        // Configure the cell...
-
-        return cell
-    }
-    */
-
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(tableView: UITableView, canEditRowAtIndexPath indexPath: NSIndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
-    }
-    */
-
-    /*
-    // Override to support editing the table view.
-    override func tableView(tableView: UITableView, commitEditingStyle editingStyle: UITableViewCellEditingStyle, forRowAtIndexPath indexPath: NSIndexPath) {
-        if editingStyle == .Delete {
-            // Delete the row from the data source
-            tableView.deleteRowsAtIndexPaths([indexPath], withRowAnimation: .Fade)
-        } else if editingStyle == .Insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
-    }
-    */
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(tableView: UITableView, moveRowAtIndexPath fromIndexPath: NSIndexPath, toIndexPath: NSIndexPath) {
-
-    }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(tableView: UITableView, canMoveRowAtIndexPath indexPath: NSIndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
-*/
-
-}
-
-/*
-extension PersonalUserViewController {
+    
     func generateRandomData() -> [[UIColor]] {
         let numRows = 20
         let numItemsPerRow = 15
         
         return (0..<numRows).map { _ in
             return (0..<numItemsPerRow).map { _ in UIColor.randomColor() }
+        }
     }
 }
 
+extension PersonalUserViewController: UICollectionViewDelegate, UICollectionViewDataSource {
+    func collectionView(collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return model[collectionView.tag].count
+    }
+    
+    func collectionView(collectionView: UICollectionView, cellForItemAtIndexPath indexPath: NSIndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCellWithReuseIdentifier("Personal User Collection View Cell", forIndexPath: indexPath)
+        cell.backgroundColor = model[collectionView.tag][indexPath.item]
+        return cell
+    }
+    /*
+    func generateRandomData() -> [[UIColor]] {
+        let numRows = 20
+        let numItemsPerRow = 15
+        
+        return (0..<numRows).map { _ in
+            return (0..<numItemsPerRow).map { _ in UIColor.randomColor() }
+        }
+    }
+    */
 
-extension UIColor {
+}
+/*
+private extension PersonalUserViewController {
+    func generateRandomData() -> [[UIColor]] {
+        let numRows = 20
+        let numItemsPerRow = 15
+        
+        return (0..<numRows).map { _ in
+            return (0..<numItemsPerRow).map { _ in UIColor.randomColor() }
+        }
+    }
+}*/
+
+private extension UIColor {
     class func randomColor() -> UIColor {
         let hue = CGFloat(arc4random() % 100) / 100
         let saturation = CGFloat(arc4random() % 100) / 100
@@ -135,4 +81,4 @@ extension UIColor {
         
         return UIColor(hue: hue, saturation: saturation, brightness: brightness, alpha: 1.0)
     }
-}*/
+}
