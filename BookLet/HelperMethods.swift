075c41c669c8a5fd76a40295ad3d0f394fbfa788
Gina M. Stalica <gstalica@searles224-17.bowdoin.edu>
2016-05-02 19:05:21 -0400

    scrolling

---
+++ BookLet/HelperMethods.swift
@@ -0,0 +1,13 @@
+//
+//  HelperMethods.swift
+//  BookLet
+//
+//  Created by Gina M. Stalica on 5/2/16.
+//  Copyright © 2016 Caroline Pierce and Gina Stalica. All rights reserved.
+//
+
+import UIKit
+
+class HelperMethods: NSObject {
+
+}
