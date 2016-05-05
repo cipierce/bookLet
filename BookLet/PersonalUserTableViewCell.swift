f16ad2beb06c5aa262d1179d973d899a38a9b847
Caroline Pierce <cpierce@searles224-06.bowdoin.edu>
2016-05-05 11:41:05 -0400

    user page

---
+++ BookLet/PersonalUserTableViewCell.swift
@@ -0,0 +1,35 @@
+//
+//  PersonalUserTableViewCell.swift
+//  BookLet
+//
+//  Created by Gina Stalica on 4/26/16.
+//  Copyright © 2016 Caroline Pierce and Gina Stalica. All rights reserved.
+//
+
+import UIKit
+
+class PersonalUserTableViewCell: UITableViewCell {
+    
+    @IBOutlet weak var collectionView: UICollectionView!
+    
+}
+
+extension PersonalUserTableViewCell {
+    
+    func setCollectionViewDataSourceDelegate <D: protocol<UICollectionViewDataSource, UICollectionViewDelegate>>(dataSourceDelegate: D, forRow row: Int) {
+        collectionView.delegate = dataSourceDelegate
+        collectionView.dataSource = dataSourceDelegate
+        collectionView.tag = row
+        collectionView.reloadData()
+    }
+    
+    var collectionViewOffset: CGFloat {
+        get {
+            return collectionView.contentOffset.x
+        }
+        set {
+            collectionView.contentOffset.x = newValue
+        }
+    }
+    
+}
---
+++ BookLet/PersonalUserViewController.swift
@@ -0,0 +1,58 @@
+//
+//  PersonalUserViewController.swift
+//  BookLet
+//
+//  Created by Gina Stalica on 4/26/16.
+//  Copyright © 2016 Caroline Pierce and Gina Stalica. All rights reserved.
+//
+
+import UIKit
+
+class PersonalUserViewController: UITableViewController {
+    
+    let model = generateData()
+    var storedOffsets = [Int: CGFloat]()
+    
+    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return model.count
+    }
+    
+    override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCellWithIdentifier("Personal User Table View Cell", forIndexPath: indexPath)
+        
+        return cell
+    }
+    
+    override func tableView(tableView: UITableView, willDisplayCell cell: UITableViewCell, forRowAtIndexPath indexPath: NSIndexPath) {
+        guard let tableViewCell = cell as? PersonalUserTableViewCell else { return }
+        
+        tableViewCell.setCollectionViewDataSourceDelegate(self, forRow: indexPath.row)
+        tableViewCell.collectionViewOffset = storedOffsets[indexPath.row] ?? 0
+    }
+    
+    override func tableView(tableView: UITableView, didEndDisplayingCell cell: UITableViewCell, forRowAtIndexPath indexPath: NSIndexPath) {
+        guard let tableViewCell = cell as? PersonalUserTableViewCell else { return }
+        
+        storedOffsets[indexPath.row] = tableViewCell.collectionViewOffset
+    }
+    
+}
+
+extension PersonalUserViewController: UICollectionViewDelegate, UICollectionViewDataSource {
+    func collectionView(collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return model[collectionView.tag].count
+    }
+    
+    func collectionView(collectionView: UICollectionView, cellForItemAtIndexPath indexPath: NSIndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCellWithReuseIdentifier("Personal User Collection View Cell", forIndexPath: indexPath)
+        cell.backgroundColor = model[collectionView.tag][indexPath.item]
+        return cell
+    }
+    
+    func collectionView(collectionView: UICollectionView, didDeselectItemAtIndexPath indexPath: NSIndexPath) {
+        print("collection view at row \(collectionView.tag) selected index path \(indexPath)")
+    }
+
+
+}
